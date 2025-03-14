4887ce579e87ed0a6df370f7fc5ee3ad20fefe96
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-03-13 22:24:10 -0600

    add operadores

---
+++ Java/Operadores/src/OperadoresAritmeticos.java
@@ -0,0 +1,26 @@
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        System.out.println("***** Operadores aritmeticos *****");
+        int a = 5,  b= 3 , resultado = 0;
+        //suma
+        resultado = a+b;
+        System.out.println("Resultado suma = " +resultado);
+        //Resta
+        resultado =a-b;
+        System.out.println("Resultado resta = "+resultado);
+        //Multipliacion
+        resultado = a*b;
+        System.out.println("Resultado multiplicacion = "+resultado);
+        //Division
+        resultado= a/b;
+        System.out.println("Resultado divicion = "+resultado);
+        //Residuo de division
+        resultado = a%b;
+        System.out.println("Resuldado de residuo = "+resultado);
+    }
+
+
+
+
+}
