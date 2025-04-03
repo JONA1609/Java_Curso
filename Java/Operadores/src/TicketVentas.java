2f92aa5cdc53d198ead5bef18f9a16106aec5133
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-02 22:19:00 -0600

    add descuento

--- Java/Operadores/src/TicketVentas.java
+++ Java/Operadores/src/TicketVentas.java
@@ -17,8 +17,18 @@ public class TicketVentas {
         System.out.print("Precio manzanas: ");
         var precioManzanas = Double.parseDouble(consola.nextLine());
 
+        //Aplicar descuento
+        System.out.println("¿Se aplicara algun descuento (%)?: ");
+        var descuentoPorcentaje = Integer.parseInt(consola.nextLine());
+
         //Calculo del subtotal sin impuestos
-        var subTotal= precioPan + precioPan + precioHuevo + precioHuevo;
+        var subTotal= precioLeche + precioPan + precioHuevo + precioHuevo + precioManzanas;
+
+        //calcular descuento
+        var descuento = subTotal *(descuentoPorcentaje/100.0);
+
+        //Subtotal con descuento
+        var subtotalConDescuento= subTotal-descuento;
 
         //Calculo iva o impuesto
         var impuestos= subTotal * 0.16;
@@ -29,7 +39,7 @@ public class TicketVentas {
         //Imprimir ticket
         System.out.printf("""
                 Subtotal:$%.2f
-                Impuesto (16%%):$%.2f 
+                Impuesto (16%%):$%.2f
                 Costo Total: $%.2f
                 """, subTotal, impuestos, costoTotal);
     }
