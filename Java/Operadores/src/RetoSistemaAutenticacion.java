7c5887e6ecee2200b02c743e8e38cf4ce5872e69
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-03 13:42:44 -0600

    mejoras a codigo descuento

---
+++ Java/Operadores/src/RetoSistemaAutenticacion.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class RetoSistemaAutenticacion {
+
+    public static void main(String[] args) {
+        System.out.println("*****  Sistema de autenticacion de usuarios  *****");
+        final String usuario= "alberto03";
+        final String contrasenia= "nomelase";
+        var consola = new Scanner(System.in);
+
+        //Solicitamos usuario y contraseña
+        System.out.println("Ingresa el usuario: ");
+        var usuario2 = consola.nextLine();
+        System.out.println("Ingresa la contraseña: ");
+        var contrasenia2 = consola.nextLine();
+
+        //Comparamos las cadenas solicitadas
+        //Para confirmar que la informacion es la correcta
+        var usuarioCorrecto = usuario.equals(usuario2)&&
+                contrasenia.equals(contrasenia2);
+
+        //imprimimos el resultado
+
+
+
+
+
+
+    }
+
+
+
+
+}
