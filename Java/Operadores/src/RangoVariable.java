efbb144776ddc8ee536c8d4395768402cf4c1c34
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-02 20:00:14 -0600

    add rango variable

---
+++ Java/Operadores/src/RangoVariable.java
@@ -0,0 +1,29 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class RangoVariable {
+    public static void main(String[] args) {
+        var consola = new Scanner(System.in);
+        boolean estaDentroRango;
+        do {
+            System.out.println("Proporciona un dato entero: ");
+            var dato = Integer.parseInt(consola.nextLine());
+
+            //Revisamos si esta dentro del rango (entre 1 y 10)
+            estaDentroRango = dato>= 1 && dato <= 10;
+            System.out.println("variable dentro de rango (1 y 10 )? "+ estaDentroRango);
+
+            // Revisar la longitud inversa se el dato esta fuera de rango
+            var estaFueraRango = !(dato >= 1 && dato <= 10);
+            System.out.println("Variable fuera de rango (1 y 10)" + estaFueraRango);
+
+
+        }while (estaDentroRango||estaDentroRango);
+
+
+
+
+
+    }
+
+}
