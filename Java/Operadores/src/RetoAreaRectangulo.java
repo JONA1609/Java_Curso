61778239b8b61f1cb7b0a41a02aef1e39290e013
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-03 18:55:37 -0600

    add reto area rectangulo 1

--- Java/Operadores/src/RetoAreaRectangulo.java
+++ Java/Operadores/src/RetoAreaRectangulo.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class RetoAreaRectangulo {
+    public static void main(String[] args) {
+        System.out.println("*****  Area y perimetro de un Rectangulo");
+        var consola = new Scanner(System.in);
+
+
+        System.out.print("Proporciona la altura del rectangulo: ");
+        var altura = Integer.parseInt(consola.nextLine());
+        System.out.print("Proporciona la base del rectangulo: ");
+        var base = Integer.parseInt(consola.nextLine());
+
+        var area = base*altura;
+        var perimetro = 2*(base+altura);
+        System.out.println("El area del rectangulo es: "+area);
+        System.out.println("El perimetro del rectangulo es: "+perimetro);
+
+
+
+
+    }
+
 }
