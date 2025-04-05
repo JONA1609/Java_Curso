264459fe4a3db49c0ea62ad14915232144cc6c25
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-04 19:39:27 -0600

    add precedencia aperadores

---
+++ Java/Operadores/src/PrecedenciaOperadores.java
@@ -0,0 +1,22 @@
+public class PrecedenciaOperadores {
+    public static void main(String[] args) {
+        //Precedencia de operadores en java
+        //1 Parentecis y corchetes
+        //2. OPeradores Unarios -,++,--
+        //3. Operadores aritmeticos *, / y %
+        //4. Operadores aritmeticos +, -
+        //5. Relacionales <, <=, >, >=
+        //6. Equivalencia == y !=
+        //7. Logicos && y ||
+        //8. Asignacion =, +=, -=, *=, etc
+
+        // Expresion
+    var a = 12/ 3 + 2 * 3 - 1;
+        System.out.println("a = "+a);
+
+
+    }
+
+
+
+}
