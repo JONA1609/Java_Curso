6cace69a7c9b952889da86f18591dc798cb2caea
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-04 20:22:39 -0600

    condicionales 1.1

---
+++ Java/Condicionales/src/SentenciaIf.java
@@ -0,0 +1,18 @@
+public class SentenciaIf {
+    public static void main(String[] args) {
+        //Uso de la sentencia if
+        var edad =16;
+
+        if (edad >=18){
+            System.out.println("Eres mayor de edad");
+        }
+        else if (edad >= 13 && edad <18){
+            System.out.println("Eres un adolecente");
+        } else {
+            System.out.println("Eres un niño");
+            
+        }
+
+    }
+
+}
