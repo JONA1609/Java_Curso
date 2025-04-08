f911b7bb65e30449381615c8b4e2f9f9707b0072
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-07 19:59:37 -0600

    sitema Descuentos 1.5

--- Java/Condicionales/src/SistemaDescuentos.java
+++ Java/Condicionales/src/SistemaDescuentos.java
@@ -1,4 +1,3 @@
-import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class SistemaDescuentos {
@@ -15,18 +14,18 @@ public class SistemaDescuentos {
         //calcular descuento y mostrar en pantalla
         //si no cumple las primeras condiciones hacer invitacion de volverse
         //mienbro de la tienda
-
+        var descuento= 0.0;
         if (montoCompra>=1000 && membrecia){
             System.out.println("Tienes un desscuento del 10%");
-            var descuento1 = montoCompra*0.1;
-            System.out.println("Cantidad a descontar: "+ descuento1);
-            var totalFinal = montoCompra - descuento1;
+             descuento = montoCompra*0.1;
+            System.out.println("Cantidad a descontar: "+ descuento);
+            var totalFinal = montoCompra - descuento;
             System.out.println("Total de compra: "+totalFinal );
         }else if (membrecia && montoCompra<1000) {
             System.out.println("Tienes un descuento del 5%");
-            var descuento2 = montoCompra * 0.05;
-            System.out.println("Cantida a descontar"+ descuento2);
-            var totalFinal = montoCompra - descuento2;
+             descuento = montoCompra * 0.05;
+            System.out.println("Cantida a descontar"+ descuento);
+            var totalFinal = montoCompra - descuento;
             System.out.println("El total con descuento es: "+ totalFinal);
         }else {
             System.out.println("tu descuento es del 0%");
