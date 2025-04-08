a158922124867f841e10343abfb69cd1289898ff
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-07 21:17:07 -0600

    sitema Descuentos 1.8

--- Java/Condicionales/src/SistemaDescuentos.java
+++ Java/Condicionales/src/SistemaDescuentos.java
@@ -6,7 +6,7 @@ public class SistemaDescuentos {
         final int MONTO_MINIMO = 1000;
         var consola = new Scanner(System.in);
 
-        System.out.print("Proporciona el monto de tu compra; ");
+        System.out.print("Proporciona el monto de tu compra: ");
         var montoCompra = Double.parseDouble(consola.nextLine());
         System.out.print("¿Tienes mambresia de la tienda (true/false)? ");
         var membrecia = Boolean.parseBoolean(consola.nextLine());
@@ -18,24 +18,27 @@ public class SistemaDescuentos {
         if (montoCompra>=MONTO_MINIMO && membrecia){
              descuento =0.1;
         }else if (membrecia && montoCompra<MONTO_MINIMO) {
-             descuento = montoCompra * 0.05;
+             descuento = 0.05;
         }else {
            descuento= 0;
         }
 
         if(descuento!=0) {
-           var descuuentoCompra = montoCompra*descuento;
-           var totalCompra = descuuentoCompra+montoCompra;
+           var descuuentoCompra = montoCompra * descuento;
+           var totalCompra =  montoCompra - descuuentoCompra;
             System.out.printf("""
                     **** Felicidas tienes un descuento ****
-                      Descuento de : %.0f
-                      Total a pagar: %2f
+                      Descuento de :----------- $%.02f
+                      Total a pagar:----------- $%.2f
                     """,descuento, totalCompra);
         }else {
             System.out.printf("""
-                    **** Te invitamos a obtener tu membresia ****
+                    %n**** Te invitamos a obtener tu membresia ****
                         Descuento:---------------------- %f
-                        Total a pagar:------------------ %2f
+                        Total a pagar:------------------ %.2f
+                    -----------------------------------------------
+                    *****¿Que esperas?, Obten tu MEMBRECIA!!!!*****
+                    
                     """,descuento, montoCompra);
         }
 
