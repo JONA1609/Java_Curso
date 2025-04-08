996129c8dfa942859286c0efbd61fe2f84b10630
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-07 23:04:26 -0600

    Operador ternario 1.3

--- Java/Condicionales/src/OperadorTernario.java
+++ Java/Condicionales/src/OperadorTernario.java
@@ -9,11 +9,14 @@ public class OperadorTernario {
         //Condicion ? expe1 : exp2
 
         //Determinar si un numero es par o no
-        var numero = new Random();
-        var numero1= numero.nextInt(100);
+        var random = new Random();
+        var numero1= random.nextInt(100);
 
         var resultado = (numero1%2 == 0)? "Par": "Impar";
         System.out.println("El numero "+numero1+ " es "+ resultado);
 
+        var edad = random.nextInt(18);
+        resultado= (edad>12)?"eres un adolecente": "Eres un niño";
+        System.out.println(resultado+" Porque tu edad es de "+edad+ " Años");
     }
 }
