36366a4b318e25b2fa21a65effe3cce86d0706a7
Jonathan160993 <jonathan_aaron93@hotmail.com>
2025-04-15 11:24:39 -0600

    add Identifica Estaciones 1.2

--- Java/Condicionales/src/Estaciones.java
+++ Java/Condicionales/src/Estaciones.java
@@ -5,7 +5,7 @@ public class Estaciones {
         System.out.println("***** Identifica las estaciones del año *****");
         var consola= new Scanner(System.in);
 
-        System.out.println("Proporciona un mes del año: ");
+        System.out.print("Proporciona un mes del año: ");
         var mes = Integer.parseInt(consola.nextLine());
         
         if (mes==1|| mes==2 || mes==12){
@@ -15,9 +15,10 @@ public class Estaciones {
         } else if (mes==6||mes==7||mes==8) {
             System.out.println("La estacion del año segun el mes es Verano :)");
         }else if (mes==9||mes==10||mes==11) {
-            System.out.println("La estacion del año segun el mes es Verano :)");
+            System.out.println("La estacion del año segun el mes es Otoño :)");
+        }else{
+            System.out.println("El mes que ingresaste no existe, vuelve a intentar");
         }
-
 
     }
 
